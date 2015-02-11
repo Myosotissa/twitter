125d0788637b31db8d22187532116d0a726a2f7f
Myosotissa <myosotissa@gmail.com>
2015-02-11 06:38:19 +0000

    Add SSL in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'contact', to: 'static_pages#contact'
   get 'help', to: 'static_pages#help'
-  get 'contact', to: 'static_pages#contact' 
   get 'about', to: 'static_pages#about'
+  get 'signup', to: 'users#new'
+  get 'signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
   
 end
