e9dbb541d2818bc82c57e1b8da8884cef50eac14
Myosotissa <myosotissa@gmail.com>
2015-02-13 07:33:25 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,15 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-=======
-
->>>>>>> sign-in-out
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   def new
     @user = User.new
   end
   
-<<<<<<< HEAD
-  def show 
-    @user = User.find(params[:id])
+  def index
+    @users = User.paginate(page: params[:page])
   end
-  def create
-    @user = User.new(user_params)
-    if @user.save == true 
-=======
+
   def show
     @user = User.find(params[:id])
   end
@@ -23,29 +18,53 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save == true
->>>>>>> sign-in-out
+
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
-<<<<<<< HEAD
-end
-  end
-  private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-  
-end
-=======
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+   
+    if @user.update(user_params) == true
+       flash[:success] = "Редактирование прошло успешно!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+  
   private
+  def signed_in_user
+    if not signed_in? 
+      redirect_to signin_url notice: "Пожалуйста войдите в систему"
+  end
+  end
+  def correct_user
+    if current_user.id != params[:id].to_i
+      redirect_to root_url, notice: "Отказано в доступе"
+    end
+  end
   
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                                  :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-end
->>>>>>> sign-in-out
+
+  def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+  end
+  
